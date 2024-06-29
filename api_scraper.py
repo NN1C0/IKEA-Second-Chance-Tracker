a063b94f7b9ae1f1896ec3a151de90dcaf3823a0
Nico Meier <nicomeier3@live.de>
2024-06-29 14:29:54 +0200

    Added error cathing to the api scraper

--- api_scraper.py
+++ api_scraper.py
@@ -19,29 +19,35 @@ def updateStoresInDatabase():
     engine = database_handler.getDatabaseEngine()
 
     with Session(engine) as session:
-        session.execute(text("TRUNCATE TABLE offers"))
-        for c in countries:
-            stores = ikea_stores.getStoreIds([c["id"]])
-            country_id_stmt = select(Countries.id).where(Countries.locale == c["id"])
-            country_id = session.execute(country_id_stmt).first()
-            
-            for s in stores:
-                insert_stmt = insert(Stores).values(store_id=s["id"],
-                                                    country=country_id[0],
-                                                    name=s["name"],
-                                                    display_name = s["display_name"])
-                on_duplicate_key_stmt = insert_stmt.on_duplicate_key_update(
-                    store_id=insert_stmt.inserted.store_id
-                )
-                session.execute(on_duplicate_key_stmt)
+        try:
+            session.execute(text("TRUNCATE TABLE offers"))
+            for c in countries:
+                stores = ikea_stores.getStoreIds([c["id"]])
+                country_id_stmt = select(Countries.id).where(Countries.locale == c["id"])
+                country_id = session.execute(country_id_stmt).first()
+                
+                for s in stores:
+                    insert_stmt = insert(Stores).values(store_id=s["id"],
+                                                        country=country_id[0],
+                                                        name=s["name"],
+                                                        display_name = s["display_name"])
+                    on_duplicate_key_stmt = insert_stmt.on_duplicate_key_update(
+                        store_id=insert_stmt.inserted.store_id
+                    )
+                    session.execute(on_duplicate_key_stmt)
 
-            updateArticlesInDatabase(session, c["id"], stores)
-            session.commit()
+                updateArticlesInDatabase(session, c["id"], stores)
+                session.commit()
+        except Exception as e:
+            print(e)
 
 def updateArticlesInDatabase(session, country_locale, stores):
-    store_ids = [s["id"] for s in stores]
-    products = second_chance_search.searchSecondChance("", country_locale, store_ids, debug=True)
-    session.execute(insert(Offers), products)
+    try:
+        store_ids = [s["id"] for s in stores]
+        products = second_chance_search.searchSecondChance("", country_locale, store_ids, debug=True)
+        session.execute(insert(Offers), products)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
